ccee4582770d42b566923321b717e508bafaa5bd
Ludolph Neethling <vsc@ludo.co.za>
2013-02-08 23:26:35 +0200

    deleted everything

--- index.md
+++
@@ -1,7 +0,0 @@
----
-layout: default
-title: Birth, death, every second counts.
----
-
-<h4 class="center"> Birth, Death, every second counts. </h4>
-
--- js/main.js
+++
@@ -1,9 +0,0 @@
-        $(document).ready(function() {
-    var over = "64px";
-    var out = "48px";
-    $(".hover").mouseover(function() {
-        $(this).animate({ width: over , height: over  }, "fast");
-    }).mouseout(function() {
-        $(this).animate({ width: out, height: out }, "fast");    
-    });
-	  });
